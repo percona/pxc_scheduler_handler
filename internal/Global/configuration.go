c59975add9e5b3452446a7eb62a530672d700ea5
Marco Tusa <tusamarco@gmail.com>
2021-05-03 18:03:26 +0200

    add Copyright

--- internal/Global/configuration.go
+++ internal/Global/configuration.go
@@ -1,3 +1,21 @@
+
+/*
+ * Copyright (c) Marco Tusa 2021 - present
+ *                     GNU GENERAL PUBLIC LICENSE
+ *                        Version 3, 29 June 2007
+ *
+ *  Copyright (C) 2007 Free Software Foundation, Inc. <https://fsf.org/>
+ *  Everyone is permitted to copy and distribute verbatim copies
+ *  of this license document, but changing it is not allowed.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
 package Global
 
 import (

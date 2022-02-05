95bda9f37adee46274791112916e14eb1f7721e1
Marco Tusa <tusamarco@gmail.com>
2022-02-05 13:08:45 +0100

    fixing version tag

--- pxc_scheduler_handler.go
+++ pxc_scheduler_handler.go
@@ -30,7 +30,7 @@ import (
 	global "pxc_scheduler_handler/internal/Global"
 )
 
-var pxcSchedulerHandlerVersion = "1.3.2"
+var pxcSchedulerHandlerVersion = "1.3.3"
 
 /*
 Main function must contain only initial parameter, log system init and main object init
